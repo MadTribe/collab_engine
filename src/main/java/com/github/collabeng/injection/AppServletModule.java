eb65fdd7963f99ab22a2d8fc362fa790029782a3
MadTribe <MadTribe@users.noreply.github.com>
2015-02-22 14:51:12 +0800

    Work in progress

---
+++ src/main/java/com/github/collabeng/injection/AppServletModule.java
@@ -0,0 +1,9 @@
+package com.github.collabeng.injection;
+
+import com.google.inject.servlet.ServletModule;
+
+/**
+ * Created by paul.smout on 21/02/2015.
+ */
+public class AppServletModule extends ServletModule {
+}
