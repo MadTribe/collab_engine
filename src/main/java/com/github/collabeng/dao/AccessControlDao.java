f7a75d863e4a630a8609541ad2767383069aa414
MadTribe <MadTribe@users.noreply.github.com>
2015-02-27 08:25:35 +0800

    as a user I can own a plan

---
+++ src/main/java/com/github/collabeng/dao/AccessControlDao.java
@@ -0,0 +1,22 @@
+package com.github.collabeng.dao;
+
+/**
+ * Created by paul.smout on 26/01/2015.
+ */
+
+import com.github.collabeng.domain.OwnedEntity;
+
+import java.util.logging.Logger;
+
+import static com.google.common.collect.Lists.asList;
+import static java.lang.String.format;
+
+
+public abstract class AccessControlDao<T extends OwnedEntity> extends BaseDao<T>{
+
+    private static final Logger LOG = Logger.getLogger(AccessControlDao.class.getName());
+
+    // TODO add in access controlled versions of super methods
+    // TODO listAll should add current owner as where criteria. [t:1h]
+    // TODO work out how to share query criteria between this and BaseDao (Decorator maybe)
+}
---
+++ src/main/java/com/github/collabeng/domain/OwnedEntity.java
@@ -0,0 +1,7 @@
+package com.github.collabeng.domain;
+
+/**
+ * Created by paul.smout on 27/02/2015.
+ */
+public class OwnedEntity {
+}
